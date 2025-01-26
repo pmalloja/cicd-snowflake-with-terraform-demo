e38af8c4d7061e073bffc49c7588af5af1d0bc61
pmalloja <mallojalap@gmail.comm>
2025-01-25 20:48:54 -0500

    changed role

--- staging/main.tf
+++ staging/main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "snowflake" {
   username    = "tf-snow"
   account     = "axb59546"
-  role        = "TF_DEMO_READER"
+  role        = "SYSADMIN"
   private_key = var.snowflake_private_key
 }
 
