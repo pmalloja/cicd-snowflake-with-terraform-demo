bc3b6b59e2b563095b27ea2b4dcba7ddd83f01f2
pmalloja <mallojalap@gmail.comm>
2025-01-29 06:18:39 -0500

    updated env1

--- staging/main.tf
+++ staging/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "snowflake" {
   username    = "tf-snow"
-  account     = "axb59546"
+  account     = "nhvnbka-axb59546"
   role        = "TF_DEMO_READER"
   private_key = var.snowflake_private_key
 }
