b5c499128a19cfb219893c44568cf92bd1734e67
pmalloja <mallojalap@gmail.comm>
2025-01-30 23:08:57 -0500

    stat

--- staging/main.tf
+++ staging/main.tf
@@ -11,6 +11,7 @@ terraform {
     key     = "terraform-staging.tfstate"
     region  = "us-east-1"
     encrypt = true
+
     #role_arn  = "arn:aws:iam::225989372755:role/terraform-s3-backend-access-role"
   }
 }
