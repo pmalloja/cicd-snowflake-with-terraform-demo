6832bec059e477452140ab212f45c66f143170e6
pmalloja <mallojalap@gmail.comm>
2025-01-30 23:48:54 -0500

    e1

--- staging/main.tf
+++ staging/main.tf
@@ -1,11 +1,10 @@
 terraform {
- 
- # required_providers {
-  #  snowflake = {
-   #   source  = "Snowflake-Labs/snowflake"
-  #    version = "0.67.0"
- #   }
-#  } 
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.67.0"
+    }
+  }
 
   backend "s3" {
     bucket  = "snowgittfpoc"
